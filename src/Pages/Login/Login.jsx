8cbbca579de668d96ad957ca7e1237063522dd23
Rasel Sharif <raselsharif9@gmail.com>
2023-09-29 21:21:48 +0600

    added github auth

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,12 +1,19 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../../firebase/firebase.config";
 import { useState } from "react";
 const Login = () => {
   const [user, setUser] = useState(null);
-  const provider = new GoogleAuthProvider();
+  const goggleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
   const auth = getAuth(app);
   const handleGoogleLogin = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, goggleProvider)
       .then((result) => {
         const googleUser = result.user;
         console.log(googleUser);
@@ -17,16 +24,59 @@ const Login = () => {
         console.log(errorSMS);
       });
   };
+  const handleGithubLogin = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        const githuUser = result.user;
+        setUser(githuUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleSignOut = () => {
+    console.log("signout");
+    signOut(auth)
+      .then(() => {
+        setUser("");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="flex items-center justify-center h-screen">
       <div>
-        <button
-          onClick={handleGoogleLogin}
-          className="py-2 px-3 bg-slate-900 text-white"
-        >
-          Login with google
-        </button>
-        <div>user: {user?.displayName}</div>
+        {user ? (
+          <button
+            onClick={handleSignOut}
+            className="py-2 px-3 bg-slate-900 text-white"
+          >
+            sign out
+          </button>
+        ) : (
+          <div>
+            <button
+              onClick={handleGoogleLogin}
+              className="py-2 px-3 bg-slate-900 text-white mr-2"
+            >
+              Login with google
+            </button>
+            <button
+              onClick={handleGithubLogin}
+              className="py-2 px-3 bg-slate-900 text-white mr-2"
+            >
+              Login with gitHub
+            </button>
+          </div>
+        )}
+
+        {user && (
+          <div>
+            <div>User: {user?.displayName}</div>
+            <div>Email: {user?.email}</div>
+          </div>
+        )}
       </div>
     </div>
   );
